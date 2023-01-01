5897ccb877f1df0699cb4d7bfa3514e87dd3b730
JohnCampi <81334006+JohnCampi@users.noreply.github.com>
2023-01-01 15:47:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const button = document.querySelectorAll(".btns");
-const clear = document.querySelector("#clear");
-const words = document.querySelector("#words");
+const div = document.querySelector("#letterButtonDiv");
+const shuffleBtn = document.querySelector("#shuffle");
 const enter = document.querySelector("#enter");
-const backspace = document.querySelector("#backspace");
+const deleteButton = document.querySelector("#deleteButton");
+const clear = document.querySelector("#clear");
+const letterDiv = document.querySelector("#letterDiv");
 const ul = document.querySelector("ul");
 const pScore = document.querySelector("p");
-const newGame = document.createElement("button");
 
-const totalWords = 22;
+const double = "Already Found";
+const short = "Too Short";
+const notWord = "Not in List";
+const noMainLetter = "Missing main letter";
 
-let wordsFound = 0;
-let letters = "";
-
-pScore.innerText = `Words Found: ${wordsFound}/${totalWords}`;
+const letterArray = [];
+const foundWords = [];
+const mainLetter = "d";
 
 const list = [
   "hydrant",
@@ -38,87 +40,121 @@ const list = [
   "tardy",
   "yard",
 ];
-const array = [];
+let letters = "";
+let totalWordsFound = 0;
+let totalWords = 0;
+for (let i = 0; i <= list.length; i++) {
+  totalWords = i;
+}
+pScore.innerText = `Words Found: ${totalWordsFound}/${totalWords}`;
 
-for (let btn of button) {
-  btn.addEventListener("click", () => {
-    // console.log("clicked", btn.innerText);
-    letters = letters + btn.innerText;
-    words.innerText = letters[0].toUpperCase() + letters.substring(1);
+const score = () => {
+  totalWordsFound += 1;
+  pScore.innerText = `Words Found: ${totalWordsFound}/${totalWords}`;
+};
+
+const disable = () => {
+  if (totalWordsFound === totalWords) {
+    enter.disabled = true;
+    clear.disabled = true;
+    deleteButton.disabled = true;
+    shuffleBtn.disabled = true;
+  }
+};
+
+const err = (e) => {
+  letterDiv.innerText = `${e}`;
+};
+
+const clearDelay = (delay) => {
+  setTimeout(() => {
+    letterDiv.innerText = "";
+    letters = "";
+  }, delay);
+};
+
+const letterShuffle = () => {
+  for (let words of list) {
+    for (let i = 0; i < words.length; i++) {
+      if (!letterArray.includes(words[i])) {
+        letterArray.push(words[i]);
+      }
+    }
+    letterArray.sort(() => Math.floor(Math.random() * 2) - 1);
+  }
+};
+
+const letterDisplay = (hide) => {
+  letterArray.forEach((element) => {
+    const idOfMainLetter = letterArray.indexOf(mainLetter);
+    const indexOfMain = letterArray.at(idOfMainLetter);
+    const button = document.createElement("button");
+    // console.log(indexOfMain);
+    div.append(button);
+    button.innerText = element;
+    button.addEventListener("click", () => {
+      letters = letters + element;
+      letterDiv.innerText = letters[0].toUpperCase() + letters.substring(1);
+    });
+    button.disabled = hide;
   });
+};
+
+if (totalWordsFound === totalWords) {
+  console.log(totalWords);
 }
 
-clear.addEventListener("click", () => {
-  words.innerText = "";
-  letters = "";
-});
-
-backspace.addEventListener("click", () => {
-  letters = letters.slice(0, -1);
-  words.innerText = words.innerText.slice(0, -1);
-});
-
-newGame.addEventListener("click", () => {
-  wordsFound = 0;
-  pScore.innerText = `Words Found: ${wordsFound}/${totalWords}`;
-  for (let btn of button) {
-    btn.disabled = false;
+const isValidWord = () => {
+  if (letterDiv.innerText.length === 0) {
+    letterDiv.innerText = "";
+  } else if (letterDiv.innerText.length < 4) {
+    err(short);
+    clearDelay(1000);
+  } else if (letterDiv.innerText.includes(mainLetter) === false) {
+    err(noMainLetter);
+    clearDelay(1000);
+  } else if (!list.includes(letters)) {
+    err(notWord);
+    clearDelay(1000);
+  } else if (foundWords.includes(letterDiv.innerText) !== true) {
+    score();
+    foundWords.push(letterDiv.innerText);
+    const newLi = document.createElement("li");
+    newLi.innerText = letterDiv.innerText;
+    ul.append(newLi);
+    letterDiv.innerText = "";
+  } else {
+    err(double);
+    clearDelay(1000);
   }
-  enter.disabled = false;
-  clear.disabled = false;
-  backspace.disabled = false;
-  words.innerText = "";
-  letters = "";
-  ul.innerHTML = "";
-  newGame.style.display = "none";
-});
+};
 
 enter.addEventListener("click", () => {
-  const newLi = document.createElement("li");
-  if (letters.includes("d") === false) {
-    words.innerText = "Missing main letter!";
-    letters = "";
-    setTimeout(() => {
-      words.innerText = "";
-    }, 1000);
-  } else if (letters.length < 4) {
-    words.innerText = "Too short!";
-    letters = "";
-    setTimeout(() => {
-      words.innerText = "";
-    }, 1000);
-  } else if (list.includes(letters) === false) {
-    words.innerText = "Not in list!";
-    letters = "";
-    setTimeout(() => {
-      words.innerText = "";
-    }, 1000);
-  } else if (array.includes(words.innerText) === false) {
-    wordsFound += 1;
-    pScore.innerText = `Words Found: ${wordsFound}/${totalWords}`;
-    array.push(words.innerText);
-    newLi.innerText = words.innerText;
-    ul.append(newLi);
-    words.innerText = "";
-    letters = "";
-    console.log(array);
-    if (wordsFound === totalWords) {
-      words.innerText = "All Words Found!";
-      for (let btn of button) {
-        btn.disabled = true;
-      }
-      enter.disabled = true;
-      clear.disabled = true;
-      backspace.disabled = true;
-      newGame.innerText = "New Game";
-      newGame.classList.add("newGame");
-      pScore.append(newGame);
-    }
-  } else if (array.includes(words.innerText) === true) {
-    words.innerText = "Already Found!";
-    setTimeout(() => {
-      words.innerText = "";
-    }, 1000);
-    letters = "";
+  clearDelay(1000);
+  isValidWord();
+  disable();
+  if (totalWordsFound === list.length) {
+    div.innerHTML = "";
+    letterDisplay(true);
   }
+  console.log(totalWords);
+  console.log(totalWordsFound);
 });
+
+shuffleBtn.addEventListener("click", () => {
+  div.innerHTML = "";
+  letterShuffle();
+  letterDisplay(false, "blue");
+});
+
+deleteButton.addEventListener("click", () => {
+  letters = letters.slice(0, -1);
+  letterDiv.innerText = letterDiv.innerText.slice(0, -1);
+});
+
+clear.addEventListener("click", () => {
+  clearDelay();
+});
+
+letterShuffle();
+letterDisplay(false, "mainLetterColor");
